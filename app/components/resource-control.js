ad2f0183d01eb70a57fc8720def1402599274809
Steve Kinney <hello@stevekinney.net>
2014-05-19 14:19:50 -0400

    Add moving circles to graph

--- app/components/resource-control.js
+++ app/components/resource-control.js
@@ -1,6 +1,19 @@
 export default Ember.Component.extend({
   
   classNames: ['resource'],
+  classNameBindings: ['isIndustryA:industry-a', 'isIndustryB:industry-b', 'isNoIndustry:industry-none'],
+  
+  isIndustryA: function () {
+    return this.get('industry') === 1;
+  }.property('industry'),
+  
+  isIndustryB: function () {
+    return this.get('industry') === 2;
+  }.property('industry'),
+  
+  isNoIndustry: function () {
+    return this.get('industry') === 0;
+  }.property('industry'),
   
   industryName: function () {
     return ['No Industry', 'Industry A', 'Industry B'][this.get('industry')];
---
+++ app/components/supply-demand-grid.js
@@ -0,0 +1,85 @@
+export default Ember.Component.extend({
+  
+  classNames: ['supply-demand-grid'],
+
+  points: [
+    [ -100, -100],
+    [ 85, 40],
+    [ 133, 130],
+    [ 178, 160],
+    [ 227, 190],
+    [ 275, 220],
+    [ 320, 248],
+    [ 370, 280],
+    [ 415, 308],
+    [ 465, 337],
+    [ 510, 337],
+    [ 555, 337],
+    [ 600, 337],
+    [ 645, 337],
+    [ 690, 337],
+    [ 1000, 337],
+  ],
+  
+  lineData: function () {
+    return this.get('points').map(function (p) {
+      return { x: p[0], y: p[1] };
+    });
+  }.property('points'),
+  
+  lineFunction: d3.svg.line().x(function(d) { return d.x; }).y(function(d) { return d.y; }).interpolate('linear'),
+  
+  didInsertElement: function () {
+    var svg = d3.select(this.$('svg').get(0));
+    this.set('svg', svg);
+    
+    var lineFunction = this.get('lineFunction');
+    var lineData = this.get('lineData');
+    
+    svg.append('path')
+      .attr('d', lineFunction(lineData))
+      .attr('stroke', 'rgba(86, 185, 197, 0.8)')
+      .attr('stroke-linecap', 'round')
+      .attr('stroke-width', 8)
+      .attr('fill', 'none');
+                            
+    var industryA = svg.append('circle').attr({
+      r: 15,
+      cx: -100,
+      cy: -100,
+      fill: 'rgba(94, 138, 185, 0.7)'
+    });
+
+    var industryB = svg.append('circle').attr({
+      r: 15,
+      cx: -100,
+      cy: -100,
+      fill: 'rgba(196, 51, 49, 0.7)'
+    });
+    
+    this.set('A', industryA);
+    this.set('B', industryB);
+  },
+  
+  updatePositionBasedOnResourcesSold: function () {
+    var a = this.translateResourcesSoldToGraphPosition(this.get('industryA'));
+    var b = this.translateResourcesSoldToGraphPosition(this.get('industryB'));
+    
+    // If A and B overlap, then make A a little bigger; otherwise, go back to
+    // the normal radius.
+    if (this.get('industryA') === this.get('industryB')) {
+      this.get('A').attr({r: 20});
+    } else {
+      this.get('A').attr({r: 15});
+    }
+    
+    this.get('A').transition().attr(a);
+    this.get('B').transition().attr(b)
+  }.observes('industryA', 'industryB'),
+  
+  translateResourcesSoldToGraphPosition: function (resourcesSold) {
+    var point = this.get('points')[resourcesSold]; 
+    return { cx: point[0], cy: point[1] }
+  }
+  
+});
