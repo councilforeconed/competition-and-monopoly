ef35c523d5ba27693e782319e7a77754e9246de9
Steve Kinney <hello@stevekinney.net>
2014-05-30 16:06:10 -0400

    Add instructions and copy

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,26 +1,32 @@
 export default Ember.ArrayController.extend({
-  
+
+  resourceOwners: 14,
+
+  isMonopoly: function () {
+    return parseInt(this.get('resourceOwners'), 10) === 1;
+  }.property('resourceOwners'),
+
   industryA: function() {
     var industry = this.filter(function(resource) {
       return resource.get('industry') === 1;
     });
     return industry.get('length');
   }.property('@each.industry'),
-  
+
   industryB: function() {
     var industry = this.filter(function(resource) {
       return resource.get('industry') === 2;
     });
     return industry.get('length');
   }.property('@each.industry'),
-  
+
   noIndustry: function() {
     var industry = this.filter(function(resource) {
       return resource.get('industry') === 0;
     });
     return industry.get('length');
   }.property('@each.industry'),
-  
+
   pricePerUnit: function () {
     var prices = [undefined, 11, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1, 1, 1, 1];
     return {
@@ -28,17 +34,23 @@ export default Ember.ArrayController.extend({
       industryB:prices[this.get('industryB')]
     };
   }.property('@each.industry', 'industryA', 'industryB'),
-  
+
   societalBenefit: function () {
     var ppu = this.get('pricePerUnit');
     var industryA = this.get('industryA') * ppu.industryA;
     var industryB = this.get('industryB') * ppu.industryB;
-    
+
     if (isNaN(industryA)) industryA = 0;
     if (isNaN(industryB)) industryB = 0;
-    
+
     var total = industryA + industryB;
     return total;
-  }.property('@each.industry', 'industryA', 'industryB', 'pricePerUnit')
-  
+  }.property('@each.industry', 'industryA', 'industryB', 'pricePerUnit'),
+
+  actions: {
+    changeResourceOwners: function (number) {
+      this.set('resourceOwners', number);
+    }
+  }
+
 });
